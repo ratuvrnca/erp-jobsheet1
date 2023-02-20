6ef19b9216ec8a39b2922699abe445822f276461
ratuvrnc <veronicaratu0@gmail.com>
2023-02-20 14:58:10 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name"          : "New Addons",
+    "version"       : "1.0",
+    "author"        : "Ratu Veronica Elyzabeth Islami",
+    "website"       : "https://www.detik.com/",
+    "category"      : "New Module",
+     "summary"      : "Ini adalah addons baru",
+    "description"   : """
+            Latihan membuat addons baru di odoo 1
+    """,
+    "depends"       : [
+        "base",
+    ],
+    "data"          : [
+
+    ],
+    "demo"          : [],
+    "test"          : [],
+    "image"         : [],
+    "qweb"          : [],
+    "css"           : [],
+    "application"   : True,
+    "insallable"    : True,
+    "auto_install"  : False,
+}
